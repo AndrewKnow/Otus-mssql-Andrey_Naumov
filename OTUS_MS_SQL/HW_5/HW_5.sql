03db03f751f6e2dc13046d3e8a137b4c763d2773
Андрей Наумов <NaumovAV1983@yandex.ru>
2024-11-29 19:14:23 +0300

    Правка + c.StockItemID) [нумерация товаров]

--- OTUS_MS_SQL/HW_5/HW_5.sql
+++ OTUS_MS_SQL/HW_5/HW_5.sql
@@ -150,7 +150,7 @@ c.StockItemID,
 c.UnitPrice,
 --a.InvoiceDate,
 max(a.InvoiceDate) over (partition by b.CustomerID, c.StockItemID) [дата],
-dense_rank() over (partition by b.CustomerID Order by c.UnitPrice desc) [нумерация товаров]
+dense_rank() over (partition by b.CustomerID Order by c.UnitPrice desc, c.StockItemID) [нумерация товаров]
 From Sales.Invoices a
 Join Sales.Customers b on b.CustomerID = a.CustomerID 
 Join Sales.InvoiceLines c on c.InvoiceID = a.InvoiceID )
