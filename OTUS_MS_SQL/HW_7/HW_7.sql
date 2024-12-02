90e56840e06bcfcb8e75e21b3395108a1c389a1e
Андрей Наумов <NaumovAV1983@yandex.ru>
2024-12-02 23:56:53 +0300

    На проверку

---
+++ OTUS_MS_SQL/HW_7/HW_7.sql
@@ -0,0 +1,55 @@
+﻿USE WideWorldImporters
+
+/*
+
+Это задание из занятия "Операторы CROSS APPLY, PIVOT, UNPIVOT."
+Нужно для него написать динамический PIVOT, отображающий результаты по всем клиентам.
+Имя клиента указывать полностью из поля CustomerName.
+
+Требуется написать запрос, который в результате своего выполнения 
+формирует сводку по количеству покупок в разрезе клиентов и месяцев.
+В строках должны быть месяцы (дата начала месяца), в столбцах - клиенты.
+
+Дата должна иметь формат dd.mm.yyyy, например, 25.12.2019.
+
+Пример, как должны выглядеть результаты:
+-------------+--------------------+--------------------+----------------+----------------------
+InvoiceMonth | Aakriti Byrraju    | Abel Spirlea       | Abel Tatarescu | ... (другие клиенты)
+-------------+--------------------+--------------------+----------------+----------------------
+01.01.2013   |      3             |        1           |      4         | ...
+01.02.2013   |      7             |        3           |      4         | ...
+-------------+--------------------+--------------------+----------------+----------------------
+*/
+
+Declare @cNames nvarchar(max), @sql nvarchar(max);
+
+-- уникальные значения CustomerName
+Select @cNames =isnull(@cNames+ ',','') + QUOTENAME(names)
+From (Select distinct c.CustomerName names
+      From Sales.Customers c) as names;
+
+--Select @cNames as cNames
+
+-- динамический SQL с PIVOT
+Set @sql = N'
+;With cteCustomerInvoices as
+(Select 
+	count(a.InvoiceID) count, 
+	c.CustomerName, 
+	convert(VARCHAR(10), a.InvoiceDate, 104) xdate
+From Sales.Invoices a Join Sales.Customers c ON a.CustomerID = c.CustomerID
+Group by c.CustomerName, convert(VARCHAR(10), a.InvoiceDate, 104))
+
+Select *
+From cteCustomerInvoices
+Pivot
+(
+    sum(count)
+    For CustomerName in (' + @cNames + ')
+) pivotTbl
+Order by  convert(date, xdate, 104) ';
+
+-- выполняем динамический SQL
+Exec sp_executesql @sql;
+
+
