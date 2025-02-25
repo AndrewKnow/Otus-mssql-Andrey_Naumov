45003893ac2f9861cdfb6e0a7fb6e5ce0ba2342e
Андрей Наумов <NaumovAV1983@yandex.ru>
2025-02-25 09:27:51 +0300

    TgmBot начало

---
+++ TgmBot/ConnectionProperties/TgmKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TgmBot.ConnectionProperties
+{
+    internal class TgmKey
+    {
+        public static string Key { get; set; }
+        public static string Bot()
+        {
+            StreamReader f = new("Key.txt");
+            if (!f.EndOfStream)
+                Key = f.ReadLine();
+            f.Close();
+            return Key;
+        }
+        public static string DB()
+        {
+            StreamReader f = new("Key.txt");
+            while (!f.EndOfStream)
+            {
+                Key = f.ReadLine();
+            }
+            f.Close();
+            return Key;
+        }
+    }
+}
---
+++ TgmBot/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using TgmBot.ConnectionProperties;
+
+namespace TgmBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Key = Password.Bot();
+            var bot = new TelegramBotClient(Key);
+        }
+    }
+}
