eb0e13c9841bb2a12666ad005aaba712cc3e5626
Андрей Наумов <NaumovAV1983@yandex.ru>
2025-04-06 12:57:05 +0300

    Cars

---
+++ OTUS_MS_SQL/Course_project/INSERT _DEMO_.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO [dbo].[Cars] (Brand, Model)
+VALUES
+('Toyota', 'Rav4'),       -- Кроссовер
+('Honda', 'CR-V'),        -- Кроссовер
+('Ford', 'Explorer'),     -- Внедорожник
+('Chevrolet', 'Tahoe'),   -- Внедорожник
+('Nissan', 'Pathfinder'), -- Внедорожник
+('Hyundai', 'Santa Fe'),  -- Кроссовер
+('Mazda', 'CX-9'),        -- Кроссовер
+('Subaru', 'Forester'),   -- Кроссовер
+('Volkswagen', 'Atlas'),  -- Кроссовер
+('BMW', 'X5'),            -- Кроссовер
+('Audi', 'Q7'),           -- Кроссовер
+('Lexus', 'GX460'),       -- Внедорожник
+('Jeep', 'Grand Cherokee'), -- Внедорожник
+('Tesla', 'Model X'),      -- Кроссовер
+('Porsche', 'Cayenne'),    -- Кроссовер
+('Land Rover', 'Discovery'), -- Внедорожник
+('Mercedes-Benz', 'GLC'),  -- Кроссовер
+('Jaguar', 'F-Pace'),      -- Кроссовер
+('Cadillac', 'XT5'),       -- Кроссовер
+('Infiniti', 'QX80');      -- Внедорожник
